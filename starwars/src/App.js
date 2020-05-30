5a9154d226f37ae0af4de85ae4780698ebebc6fa
sid2015980 <sidneybroadnax@gmail.com>
2020-05-29 19:42:53 -0700

    MVP DONE, trying stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+function App() {
+  const [card, setCard] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/api/people")
+      .then((response) => {
+        console.log(response.data.results);
+        setCard(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []); //the [] allows the data to mount once and never again, instead of constantly loading
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">⭐️⭐️⭐️⭐️⭐️React Wars⭐️⭐️⭐️⭐️⭐️</h1>
+      <form>
+        <label className="search" htmlFor="name">Search:</label>
+        <input id="name" type="text" name="textfield" placeholder="Search" />
+      </form>
+      {card.map((CharData, index) => {
+        console.log(CharData);
+        return (
+          <Character
+            key={index}
+            name={CharData.name}
+            height={CharData.height}
+            mass={CharData.mass}
+            birthyear={CharData.birth_year}
+            gender={CharData.gender}
+            homeworld={CharData.homeworld}
+          />
+        );
+      })}
     </div>
   );
 }
